d19cc334b2d11a2e2636f4e227a010feef49eb67
Andriy <andrijbuluk@gmail.com>
2026-06-30 19:11:48 +0200

    Fix: read-only images in serializers

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -42,6 +42,7 @@ class MovieSerializer(serializers.ModelSerializer):
             "genres",
             "actors",
         )
+        read_only_fields = ("id", "image")
 
 
 class MovieImageSerializer(MovieSerializer):
